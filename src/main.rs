59dbbd5fa2a0b547eb34ae0f465376c1743f1520
brokenVectors <brokenvectors@gmail.com>
2022-02-17 18:37:55 +0100

    sorted stuff into functions

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,49 @@ fn init_clue(clue: &mut String, word: &String) {
         clue.push_str("_");
     }
 }
+
+fn clear_terminal() {
+    print!("{}[2J", 27 as char);
+}
+
+fn game_over(secret_word: String) {
+    println!("Game over! You ran out of guesses. The word was: {}", secret_word);
+}
+
+fn display_game_state(clue: &String, guesses: i32) {
+    println!("Let's play some Hangman!\n{} {} guesses left!", clue, guesses);
+}
+
+fn win(user_guess: &String) {
+    println!("Nice! That's the word. You win! (The word was '{}')", user_guess);
+}
+
+fn random_word(word_list: &[&str]) -> String {
+    return String::from(word_list[rand::thread_rng().gen_range(0..word_list.len())]);
+}
+
 fn main() {
-    let WORD_LIST: [&str; 7] = ["house", "hornets", "people", "troglodyte", "personalities", "celebrity", "caffeine"];
-    let wordIndex = rand::thread_rng().gen_range(0..WORD_LIST.len());
-    let secret_word: String = String::from(WORD_LIST[wordIndex]);
+    clear_terminal();
+    let word_list: [&str; 7] = ["house", "hornets", "people", "troglodyte", "personalities", "celebrity", "caffeine"];
+    let secret_word: String = random_word(&word_list);
     let mut guesses = 6;
 
     println!("Let's play some Hangman!");
+
     let mut total_clue = String::new();
     init_clue(&mut total_clue, &secret_word);
 
     loop {
-
+        clear_terminal();
         if guesses == 0 {
-            println!("Game over! You ran out of guesses. The word was: {}", secret_word);
+            game_over(secret_word);
             break;
         }
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
+
+        let mut user_guess = String::new();
+        match io::stdin().read_line(&mut user_guess) {
             Ok(_) => {
-                input = input.trim().to_string();
+                user_guess = user_guess.trim().to_string();
             }
 
             Err(e) => {
@@ -33,16 +56,11 @@ fn main() {
             }
         }
 
-        // user's guess is input, obviously!
-        let user_guess = input;
         if user_guess.len() == 1 {
-            
-
             // display all places where user's proposed letter is
             let mut letter_is_in_word = false;
             for (i, c) in secret_word.chars().enumerate() {
-                let c_string = c.to_string();
-                if c_string == user_guess {
+                if c.to_string() == user_guess {
                     total_clue.replace_range(i..i+1, &user_guess);
                     letter_is_in_word = true;
                 }
@@ -52,14 +70,13 @@ fn main() {
             }
 
             // display current game state
-            println!("{} {} guesses left!", total_clue, guesses);
-            
+            display_game_state(&total_clue, guesses);
         }
         else {
-
             // if user guesses correctly, tell them they did
             if secret_word.eq(&user_guess) {
-                println!("Nice! That's the word. You win!");
+                display_game_state(&total_clue, guesses);
+                win(&user_guess);
                 break;
             }
             else {

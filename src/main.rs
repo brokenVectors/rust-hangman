fec33e1203b854390c2c0a822c337f935c6665e5
brokenVectors <brokenvectors@gmail.com>
2022-02-17 16:10:33 +0100

    told the user what the secret word was after losing & only subtracted guess if letter was not in word

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use std::io;
 use rand::Rng;
 
+fn init_clue(clue: &mut String, word: &String) {
+    for _ in 0..word.len() {
+        clue.push_str("_");
+    }
+}
 fn main() {
     let WORD_LIST: [&str; 7] = ["house", "hornets", "people", "troglodyte", "personalities", "celebrity", "caffeine"];
     let wordIndex = rand::thread_rng().gen_range(0..WORD_LIST.len());
-    let SECRET_WORD: &str = WORD_LIST[wordIndex];
+    let secret_word: String = String::from(WORD_LIST[wordIndex]);
     let mut guesses = 6;
 
     println!("Let's play some Hangman!");
-
     let mut total_clue = String::new();
-    for _ in 0..SECRET_WORD.len() {
-        total_clue.push_str("_");
-    }
+    init_clue(&mut total_clue, &secret_word);
 
     loop {
 
         if guesses == 0 {
-            println!("Game over! You ran out of guesses.");
+            println!("Game over! You ran out of guesses. The word was: {}", secret_word);
             break;
         }
         let mut input = String::new();
@@ -34,14 +36,19 @@ fn main() {
         // user's guess is input, obviously!
         let user_guess = input;
         if user_guess.len() == 1 {
-            guesses -= 1;
+            
 
             // display all places where user's proposed letter is
-            for (i, c) in SECRET_WORD.chars().enumerate() {
+            let mut letter_is_in_word = false;
+            for (i, c) in secret_word.chars().enumerate() {
                 let c_string = c.to_string();
                 if c_string == user_guess {
                     total_clue.replace_range(i..i+1, &user_guess);
+                    letter_is_in_word = true;
                 }
+            }
+            if !letter_is_in_word {
+                guesses -= 1;
             }
 
             // display current game state
@@ -51,7 +58,7 @@ fn main() {
         else {
 
             // if user guesses correctly, tell them they did
-            if SECRET_WORD.eq(&user_guess) {
+            if secret_word.eq(&user_guess) {
                 println!("Nice! That's the word. You win!");
                 break;
             }
